8d3a6a40bd1f82c7cdde241821efa75578c4b282
Tang, Bin <77304+tang-bin@users.noreply.github.com>
2020-07-10 14:06:23 -0700

    Sync

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,30 @@
 import path = require("path");
+import yaml = require("js-yaml");
+import fs = require("fs");
+import { util } from "./util/util";
+import { fileUtil } from "./util/fileUtil";
 
 class Config {
+
     public appRoot: string = "";
     public contentRoot: string = "";
     public staticRoot: string = ""
     public themeRoot: string = "";
-    public themeName: string = "default";
+    public themeName: string = "";
     public port: Number = 3000;
+
     constructor() {
         this.appRoot = path.resolve(__dirname, "..");
     }
     public load(): void {
-        this.contentRoot = this.trimPath("", "content");
-        this.staticRoot = this.trimPath("", "static");
-        this.themeRoot = this.trimPath("", "theme");
+        let configFilePath: string = path.resolve(this.appRoot, "config.yaml")
+            , d: any = fileUtil.exists(configFilePath) ? yaml.safeLoad(fs.readFileSync(configFilePath, 'utf8')) : {}
+            , l = (p: string) => util.readFromObject(d, p);
+
+        this.contentRoot = this.trimPath(l("global.contentRoot"), "content");
+        this.staticRoot = this.trimPath(l("global.staticRoot"), "static");
+        this.themeRoot = this.trimPath(l("theme.root"), "theme");
+        this.port = l("global.port") || 3000;
     }
 
     private trimPath(p: string, defaultPath: string): string {
--- src/util/util.ts
+++ src/util/util.ts
@@ -3,5 +3,20 @@ class Util {
         if (!a || !b) return false;
         return a.lastIndexOf(b) === a.length - b.length;
     }
+
+    public readFromObject(data: any, path: string = ""): any {
+        if (data) {
+            if (path) {
+                return path.split(".")
+                    .map((s: string) => s.trim())
+                    .filter((s: string) => s)
+                    .reduce((d: any, p: string) => {
+                        if (d && d[p] !== undefined) return d[p];
+                        else return null;
+                    }, data);
+
+            } else return data;
+        } else return null;
+    }
 }
 export const util: Util = new Util();
