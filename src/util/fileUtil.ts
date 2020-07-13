1f6589501ec4f486a3dc4b7808d3dc2012d64862
Tang, Bin <77304+tang-bin@users.noreply.github.com>
2020-07-13 08:31:34 -0700

    Sync

--- src/util/fileUtil.ts
+++ src/util/fileUtil.ts
@@ -1,8 +1,21 @@
 import fs = require("fs");
 class FileUtil {
     public exists(path: string): boolean {
+        return this.isFile(path) || this.isFolder(path);
+    }
+
+    public isFile(path: string): boolean {
         try {
-            const isFile:boolean = fs.lstatSync(path).isFile();
+            const isFile: boolean = fs.lstatSync(path).isFile();
+            return isFile;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    public isFolder(path: string): boolean {
+        try {
+            const isFile: boolean = fs.lstatSync(path).isDirectory();
             return isFile;
         } catch (err) {
             return false;
